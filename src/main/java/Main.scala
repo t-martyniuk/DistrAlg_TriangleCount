19b615f4d226583f84df1709cdeea840cbaca257
Tetiana <tetyanka.martynyuk@gmail.com>
2018-04-27 18:24:58 +0300

    Fixed output

--- src/main/java/Main.scala
+++ src/main/java/Main.scala
@@ -1,5 +1,6 @@
 import org.apache.spark.graphx.{GraphLoader, PartitionStrategy}
 import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.graphx.lib.{MyTrianglesCounter, TriangleCount}
 
 object Main {
   def main(args: Array[String]): Unit = {
@@ -8,8 +9,16 @@ object Main {
     sc.setLogLevel("ERROR")
     val graph = GraphLoader.edgeListFile(sc, "src/main/resources/followers.txt", true)
       .partitionBy(PartitionStrategy.RandomVertexCut)
-    val triangCount = graph.triangleCount().vertices
-    val tripledTriangles = triangCount.map(_._2).reduce(_ + _)
+
+    val verticeCount = TriangleCount.run(graph).vertices
+
+    val tripledTriangles = verticeCount.map(_._2).reduce(_ + _)
+
+    val myImplTriangCount = MyTrianglesCounter.runAndCalculateNumberOfTriangles(graph)
+    System.out.println("=============================================================================")
     System.out.println("Number of triangles = " + tripledTriangles/3)
+    System.out.println("=============================================================================")
+    System.out.println("My wrapped implementation returns number of triangles = " + myImplTriangCount)
+    System.out.println("=============================================================================")
  }
 }

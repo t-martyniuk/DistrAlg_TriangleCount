1d77aa14bdd844a4c6d0dca7e689ac8809bfec4c
Tetiana <tetyanka.martynyuk@gmail.com>
2018-04-27 15:45:13 +0300

    Fixed output

--- src/main/java/Main.scala
+++ src/main/java/Main.scala
@@ -5,9 +5,11 @@ object Main {
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf().setAppName("Count Triangles").setMaster("local[*]").set("spark.executor.memory", "1g")
     val sc = new SparkContext(conf)
-    val graph = GraphLoader.edgeListFile(sc, "followers.txt", true)
+    sc.setLogLevel("ERROR")
+    val graph = GraphLoader.edgeListFile(sc, "src/main/resources/followers.txt", true)
       .partitionBy(PartitionStrategy.RandomVertexCut)
     val triangCount = graph.triangleCount().vertices
-    println(triangCount.map(_._2).reduce(_ + _))
+    val tripledTriangles = triangCount.map(_._2).reduce(_ + _)
+    System.out.println("Number of triangles = " + tripledTriangles/3)
  }
 }
